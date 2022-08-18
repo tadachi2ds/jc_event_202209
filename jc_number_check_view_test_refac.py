7fb7ed52fd92bec8a39ae9fc4a1d7750af90c7a4
Takashi Adachi <tadachi2@Ta2MBA.local>
2022-08-18 23:56:12 +0900

    オブジェクト描画リファクタリング

--- jc_number_check_view_test_refac.py
+++ jc_number_check_view_test_refac.py
@@ -36,75 +36,104 @@ def screen_object_setting() :
     btn_dwn_y = 700
     num_y = 300
 
+    team_x = 350
+    num1_x = 700
+    num2_x = 1000
+    num3_x = 1300
+    team_adg_x = 35
+    team_adg_y = 250    
+    team_adg_up_y = 150
+    team_adg_down_y = 80
+    btn_adg = 50
+    sbm_x = 600
+    sbm_y = 900
+
     screen_obj = {
         'bg' : {
             'img' : load_image("img/N_BackGrand_1920_1080.png")
         },
         'team' : {
-            'imgs' : [ load_image("img/N_9_256_340.png", 150, 250) ],
+            'img'   : {
+                'imgs' : [ load_image("img/N_9_256_340.png", 150, 250) ],
+                'x' : team_x,
+                'y' : btn_up_y + team_adg_y
+            },
             'btn'   : {
                 'up' : {
                     'img'    :  load_image("img/N_Button_Up.png", 80, 80),
-                    'x' : 0,
-                    'y' : btn_up_y
+                    'x' : team_x + team_adg_x,
+                    'y' : btn_up_y + team_adg_up_y 
                 }, 
                 'down' : {
                     'img'    :  load_image("img/N_Button_Down.png", 80, 80),
-                    'x' : 0,
-                    'y' : 0
+                    'x' : team_x + team_adg_x,
+                    'y' : btn_dwn_y - team_adg_down_y
                 }, 
             }
         },
         'num1' : {
-            'imgs'  : [ load_image("img/N_1.png") ],
+            'img'   : {
+                'imgs'  : [ load_image("img/N_1.png") ],
+                'x'     : num1_x,
+                'y'     : num_y 
+            },
             'btn'   : {
                 'up' : {
-                    'img'    :  load_image("img/N_Button_Up.png", 80, 80),
-                    'x' : 0,
+                    'img'    :  load_image("img/N_Button_Up.png"),
+                    'x' : num1_x + btn_adg,
                     'y' : btn_up_y
                 }, 
                 'down' : {
-                    'img'    :  load_image("img/N_Button_Down.png", 80, 80),
-                    'x' : 0,
-                    'y' : 0
+                    'img'    :  load_image("img/N_Button_Down.png"),
+                    'x' : num1_x + btn_adg,
+                    'y' : btn_dwn_y 
                 }, 
             }
         },
         'num2' : {
-            'imgs'  : [ load_image("img/N_1.png") ],
+            'img'   : {
+                'imgs'  : [ load_image("img/N_1.png") ],
+                'x'     : num2_x,
+                'y'     : num_y 
+            },
+
             'btn'   : {
                 'up' : {
                     'img'    :  load_image("img/N_Button_Up.png"),
-                    'x' : 0,
+                    'x' : num2_x + btn_adg,
                     'y' : btn_up_y
                 }, 
                 'down' : {
                     'img'    :  load_image("img/N_Button_Down.png"),
-                    'x' : 0,
-                    'y' : 0
+                    'x' : num2_x + btn_adg,
+                    'y' : btn_dwn_y
                 }, 
             }
         },
         'num3' : {
-            'imgs'  : [ load_image("img/N_1.png") ],
+            'img'   : {
+                'imgs'  : [ load_image("img/N_1.png") ],
+                'x'     : num3_x,
+                'y'     : num_y 
+            },
             'btn'   : {
                 'up' : {
                     'img'    :  load_image("img/N_Button_Up.png"),
-                    'x' : 0,
+                    'x' : num3_x + btn_adg,
                     'y' : btn_up_y
                 }, 
                 'down' : {
                     'img'    :  load_image("img/N_Button_Down.png"),
-                    'x' : 0,
-                    'y' : 0
+                    'x' : num3_x + btn_adg,
+                    'y' : btn_dwn_y 
                 }, 
             }
         },
         'submit' : {
             'btn'   : {
                 'img'    :  load_image("img/N_Button_AnswerCheck_Normal_850_180.png"),
-                'x' : 0,
-                'y' : 0
+                'x' :   sbm_x,
+                'y' :   sbm_y
             }
         }
 
@@ -131,8 +160,25 @@ def screen_object_setting() :
     return screen_obj
 
 
-def screen_object_draw() :
-    pass
+def screen_object_draw(screen, screen_obj) :
+    screen.blit(screen_obj['bg']['img'], (0,0))
+
+    screen.blit(screen_obj['team']['img']['imgs'][0], (screen_obj['team']['img']['x'], screen_obj['team']['img']['y']))
+    screen.blit(screen_obj['num1']['img']['imgs'][0], (screen_obj['num1']['img']['x'], screen_obj['num1']['img']['y']))
+    screen.blit(screen_obj['num2']['img']['imgs'][0], (screen_obj['num2']['img']['x'], screen_obj['num2']['img']['y']))
+    screen.blit(screen_obj['num3']['img']['imgs'][0], (screen_obj['num3']['img']['x'], screen_obj['num3']['img']['y']))
+
+    screen.blit(screen_obj['team']['btn']['up']['img'], (screen_obj['team']['btn']['up']['x'], screen_obj['team']['btn']['up']['y']))
+    screen.blit(screen_obj['num1']['btn']['up']['img'], (screen_obj['num1']['btn']['up']['x'], screen_obj['num1']['btn']['up']['y']))
+    screen.blit(screen_obj['num2']['btn']['up']['img'], (screen_obj['num2']['btn']['up']['x'], screen_obj['num2']['btn']['up']['y']))
+    screen.blit(screen_obj['num3']['btn']['up']['img'], (screen_obj['num3']['btn']['up']['x'], screen_obj['num3']['btn']['up']['y']))
+
+    screen.blit(screen_obj['team']['btn']['down']['img'], (screen_obj['team']['btn']['down']['x'], screen_obj['team']['btn']['down']['y']))
+    screen.blit(screen_obj['num1']['btn']['down']['img'], (screen_obj['num1']['btn']['down']['x'], screen_obj['num1']['btn']['down']['y']))
+    screen.blit(screen_obj['num2']['btn']['down']['img'], (screen_obj['num2']['btn']['down']['x'], screen_obj['num2']['btn']['down']['y']))
+    screen.blit(screen_obj['num3']['btn']['down']['img'], (screen_obj['num3']['btn']['down']['x'], screen_obj['num3']['btn']['down']['y']))
+
+    screen.blit(screen_obj['submit']['btn']['img'], (screen_obj['submit']['btn']['x'], screen_obj['submit']['btn']['y']))
 
 
 # ----------------------------------------------------
@@ -155,69 +201,24 @@ def main() :
     screen = pygame.display.set_mode((prm['width'], prm['height']), pygame.FULLSCREEN, 32) 
     pygame.display.set_caption("NumberCheck") 
 
-    # screen_object_setting()
-
-    bg_img = load_image("img/N_BackGrand_1920_1080.png")
-
-    team = load_image("img/N_9_256_340.png", 150, 250)    
-    num1 = load_image("img/N_1.png")
-    num2 = load_image("img/N_1.png")
-    num3 = load_image("img/N_1.png")
-    
-    btn_up0 = load_image("img/N_Button_Up.png", 80, 80)
-    btn_up1 = load_image("img/N_Button_Up.png")
-    btn_up2 = load_image("img/N_Button_Up.png")
-    btn_up3 = load_image("img/N_Button_Up.png")
-    btn_dwn0 = load_image("img/N_Button_Down.png", 80, 80)
-    btn_dwn1 = load_image("img/N_Button_Down.png")
-    btn_dwn2 = load_image("img/N_Button_Down.png")
-    btn_dwn3 = load_image("img/N_Button_Down.png")
-    btn_sbmt = load_image("img/N_Button_AnswerCheck_Normal_850_180.png")
-
-    btn_up_y = 100
-    btn_dwn_y = 700
-    num_y = 300
-
-    team_x = 350
-    num1_x = 700
-    num2_x = 1000
-    num3_x = 1300
-    team_adg_x = 35
-    team_adg_up_y = 150
-    team_adg_down_y = 80
-    btn_adg = 50
+    screen_obj = screen_object_setting()
 
     font = pygame.font.Font('img/CICA-REGULAR.TTF', 60) 
-
-
 
     fullscreen_flg = True
     running = True
     while running :
         pygame.time.wait(30)
 
-        # screen_object_draw()
+        screen_object_draw(screen, screen_obj)
+
         # screen.fill((0, 20, 0, 0))
-        screen.blit(bg_img, (0,0))
-
-        screen.blit(team, (team_x, num_y + 50))
-        screen.blit(num1, (num1_x, num_y))
-        screen.blit(num2, (num2_x, num_y))
-        screen.blit(num3, (num3_x, num_y))
-        screen.blit(btn_up0, (team_x + team_adg_x, btn_up_y + team_adg_up_y ))
-        screen.blit(btn_up1, (num1_x + btn_adg, btn_up_y))
-        screen.blit(btn_up2, (num2_x + btn_adg, btn_up_y))
-        screen.blit(btn_up3, (num3_x + btn_adg, btn_up_y))
-        screen.blit(btn_dwn0, (team_x + team_adg_x, btn_dwn_y - team_adg_down_y))
-        screen.blit(btn_dwn1, (num1_x + btn_adg, btn_dwn_y))
-        screen.blit(btn_dwn2, (num2_x + btn_adg, btn_dwn_y))
-        screen.blit(btn_dwn3, (num3_x + btn_adg, btn_dwn_y))
-        screen.blit(btn_sbmt, (600, 900))
-
+        # チーム名用の枠
         pygame.draw.rect(screen, (0,0,0), (280,180,300,600), width = 10)
         pygame.draw.rect(screen, (0,0,0), (280,130,300,100), width = 0)
         team_text = font.render("チーム名", True, (255,255,255)) 
         screen.blit(team_text, [300, 160])
+
 
         for event in pygame.event.get():
              # 終了用のイベント処理
